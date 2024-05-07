969af1bb90ba95dcfce324b4d4ab5f11225e9160
BeautifulAngel246 <favouronuche@gmail.com>
2024-05-07 16:26:31 -0700

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-"""Module for number_of_subscribers function"""
-
+""" Number of subscribers """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """function that queries the Reddit API and returns the number of
-    subscribers (not active users, total subscribers) for a given subreddit"""
-
+    """ Returns the number of subscribers """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    header = {'User-Agent': 'Mozilla/5.0'}
-
-    response = requests.get(url, headers=header, allow_redirects=False)
-
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
-        return 0
+        return response.json().get("data").get("subscribers")
+    return 0
